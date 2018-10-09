af8fcb1131860111f6ff2ab8816b93c533d2e016
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-09 19:57:25 -0300

    Projeto criado

---
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
@@ -0,0 +1,12 @@
+package br.com.cenarioesolucao.cursoModelagemConceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
+	}
+}
