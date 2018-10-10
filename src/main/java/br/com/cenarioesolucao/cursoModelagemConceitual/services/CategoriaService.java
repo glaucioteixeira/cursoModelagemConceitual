9ae60b69b3365fa80213ceefa2ed566861cdb94d
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-10 12:06:12 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/CategoriaService.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Categoria;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CategoriaRepository;
+import br.com.cenarioesolucao.cursoModelagemConceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		//return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
