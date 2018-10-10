c06909b82057cfe843213dfd5d3af5e7f1630dfc
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-10 20:50:51 -0300

    Listando todas as Categorias com DTO

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/CategoriaService.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.cenarioesolucao.cursoModelagemConceitual.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,5 +41,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que já possui produtos cadastrados!");
 		}
+	}
+	
+	public List<Categoria> buscarAll() {
+		return repo.findAll();
 	}
 }
