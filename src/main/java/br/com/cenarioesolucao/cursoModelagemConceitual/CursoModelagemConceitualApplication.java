a480a082bd3c3a51586e386dc11d80a425bbca44
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-10 12:52:14 -0300

    Estado e Cidade

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Categoria;
+import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Cidade;
+import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Estado;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Produto;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CategoriaRepository;
+import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CidadeRepository;
+import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.EstadoRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
@@ -43,6 +53,19 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "MINAS GERAIS");
+		Estado est2 = new Estado(null, "SÃO PAULO");
+		
+		Cidade c1 = new Cidade(null, "UBERLÂNDIA", est1);
+		Cidade c2 = new Cidade(null, "SÃO PAULO", est2);
+		Cidade c3 = new Cidade(null, "CAMPINAS", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 }
