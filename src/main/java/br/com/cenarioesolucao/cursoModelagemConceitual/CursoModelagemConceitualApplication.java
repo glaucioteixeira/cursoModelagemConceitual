4f8cd3430186e03e9328fdc30ffd8aaa8b7c02ef
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-10 10:50:38 -0300

    Operação de instanciação criada

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
@@ -1,12 +1,30 @@
 package br.com.cenarioesolucao.cursoModelagemConceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoModelagemConceitualApplication {
+import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Categoria;
+import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoModelagemConceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "INFORMÁTICA");
+		Categoria cat2 = new Categoria(null, "ESCRITÓRIO");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
