7406b91993bc4d3c5f94e248169468e136f2d77e
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-10 17:02:11 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/CursoModelagemConceitualApplication.java
@@ -13,6 +13,7 @@ import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Cidade;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Cliente;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Endereco;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Estado;
+import br.com.cenarioesolucao.cursoModelagemConceitual.domain.ItemPedido;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Pagamento;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.PagamentoComBoleto;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CidadeReposi
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.ClienteRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.EnderecoRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.EstadoRepository;
+import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.ItemPedidoRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.PagamentoRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.PedidoRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
@@ -119,6 +124,18 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 }
