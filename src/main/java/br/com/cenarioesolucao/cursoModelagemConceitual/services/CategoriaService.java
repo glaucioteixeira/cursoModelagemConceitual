cebe6266cd3e7aed9b4853d306f3eeb66a6ab6e8
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-11 11:45:53 -0300

    Validação sintatica com Bean Validation

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/CategoriaService.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Categoria;
+import br.com.cenarioesolucao.cursoModelagemConceitual.dto.CategoriaDTO;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CategoriaRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.services.exceptions.DataIntegrityException;
 import br.com.cenarioesolucao.cursoModelagemConceitual.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 	
