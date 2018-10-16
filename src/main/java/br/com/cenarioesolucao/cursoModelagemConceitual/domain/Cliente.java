670bbe6ecc6f15084121948d0de0b912d36e17b1
Gláucio Teixeira <glaucio.teixeira@outlook.com>
2018-10-16 19:27:27 -0300

    Salvando perfis de Cliente na base de dados

--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/domain/Cliente.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/domain/Cliente.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,6 +20,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.cenarioesolucao.cursoModelagemConceitual.domain.enums.Perfil;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.enums.TipoCliente;
 
 @Entity
@@ -53,8 +56,12 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	private String senha;
 	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
+	private Set<Integer> perfis = new HashSet<>();
+	
 	public Cliente() {
-		
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente, String senha) {
@@ -65,6 +72,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipoCliente = (tipoCliente == null) ? null : tipoCliente.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -137,6 +145,14 @@ public class Cliente implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
 	}
 
 	@Override
--- src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/DBService.java
+++ src/main/java/br/com/cenarioesolucao/cursoModelagemConceitual/services/DBService.java
@@ -20,6 +20,7 @@ import br.com.cenarioesolucao.cursoModelagemConceitual.domain.PagamentoComCartao
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Pedido;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.Produto;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.enums.EstadoPagamento;
+import br.com.cenarioesolucao.cursoModelagemConceitual.domain.enums.Perfil;
 import br.com.cenarioesolucao.cursoModelagemConceitual.domain.enums.TipoCliente;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CategoriaRepository;
 import br.com.cenarioesolucao.cursoModelagemConceitual.repositories.CidadeRepository;
@@ -130,13 +131,20 @@ public class DBService {
 				TipoCliente.PESSOAFISICA, passwordEncoder.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "glaucio.teixeira@outlook.com", "31628382740", 
+				TipoCliente.PESSOAFISICA, passwordEncoder.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("99500593", "99117270"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Rua Geraldo Ramos", "170", "Casa", "Novo Santa Cecilia", "30626692", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
